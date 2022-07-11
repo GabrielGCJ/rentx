82f954752094a3eb21cbc19ea83ea38d12de0554
Gabriel Jorge <gabriel.ggcj@gmail.com>
2022-07-11 17:51:06 -0300

    conexão do banco de dados

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,16 +1,65 @@
-import { createConnection } from "typeorm";
+import { createConnection, getConnectionOptions } from 'typeorm';
 
-createConnection()
+interface IOptions {
+  host: string;
+}
 
-// import { DataSource } from "typeorm";
+getConnectionOptions().then(options => {
+  const newOptions = options as IOptions;
+  newOptions.host = 'database'; //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+  createConnection({
+    ...options,
+  });
+});
 
-// const dataSource = new DataSource({
-//     type:"postgres",
-//     host:"database", //nome dado ao serviço de banco de dados
-//     port: 5432,
-//     username:"docker",
-//     password:"ignite",
-//     database: "avalon"
-// })
 
-// dataSource.initialize();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { createConnection } from "typeorm";
+
+// createConnection()
+
+// // import { DataSource } from "typeorm";
+
+// // const dataSource = new DataSource({
+// //     type:"postgres",
+// //     host:"database", //nome dado ao serviço de banco de dados
+// //     port: 5432,
+// //     username:"docker",
+// //     password:"ignite",
+// //     database: "avalon"
+// // })
+
+// // dataSource.initialize();
+
+// // const PostgresDataSource = new DataSource({
+// //     type: "postgres",
+// //     host: "localhost",
+// //     port: 5432,
+// //     username: "test",
+// //     password: "test",
+// //     database: "test",
+    
+// // })
+
+// // PostgresDataSource.initialize();
